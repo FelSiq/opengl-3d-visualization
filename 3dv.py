2b0f815e3554b72aa1c6115fce290d1ae00b2f39
FelSiq <felipe.siqueira@usp.br>
2018-05-24 21:57:03 -0300

    Added more configuration and created pentagonal prims.

--- 3dv.py
+++ 3dv.py
@@ -1,4 +1,19 @@
 """
+	TO DO LIST:
+	- PASS ARGUMENTS CORRECTLY TO "drawObject" function
+	- IMPLEMENT KEYBOARD INPUTS
+	- DRAW OBJECT B
+	- DRAW OBJECT C
+	- FIX/IMPROVE COMMENTARIES
+	- IMPLEMENT TRANSLATION
+	- IMPLEMENT SCALE (ZOOM IN/OUT)
+	- PRINT SUBTITLES
+	- SUBMIT
+	- USE MORE PROJECTION TYPES (ORTHO, FRUSTUM, PERSPECTVE)
+"""
+
+
+"""
 	Group:
 
 	B...
@@ -10,35 +25,34 @@
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from OpenGL.GLUT import *
-
+import math
 
 """
 Configuration variables section
-
-Draw a pentadiagonal 
-function(x, y, l) { 
-    points = NULL; 
-    for (k in 72*(0:4)) points = rbind(points, c((x - l*0.5)*cos(0.4*pi*k), (y + tan(0.3*pi/2)*l)*sin(0.4*pi*k))); 
-    return (points) 
-}
 
 """
 
 WINDOW_WIDTH=1080
 WINDOW_HEIGHT=640
-X_OBJECT_ANGLE=0
-Y_OBJECT_ANGLE=0
-Z_OBJECT_ANGLE=0
-X_COORD=0
-Y_COORD=0
-Z_COORD=0
-ROTATION_INC=2
+X_OBJECT_ANGLE=30.0
+Y_OBJECT_ANGLE=30.0
+Z_OBJECT_ANGLE=30.0
+X_COORD=0.0
+Y_COORD=0.0
+Z_COORD=0.0
+ROTATION_INC=2.0
 TRANSLATION_INC=0.025
 SCALE_FACTOR_INC=0.05
 SCALE_FACTOR=1.0
 SCALE_FACTOR_MAX=1.0
 SCALE_FACTOR_MIN=0.025
 ENABLE_RENDER=True
+SHOW_AXIS=True
+OBJECT_ARGUMENTS=[1, 0.25, 0.75]
+COLOR_R=1.0
+COLOR_G=1.0
+COLOR_B=1.0
+PERSPECTIVE=0
 
 def setup():
 	"""
@@ -79,16 +93,29 @@ def inputEvents(key, x, y):
 	global X_COORD
 	global Y_COORD
 	global Z_COORD
-	global SCALE_FACTOR
 	global ENABLE_RENDER
+	global SHOW_AXIS
 
 	if key == b'+':
 		SCALE_FACTOR = min(SCALE_FACTOR_MAX, 
 				SCALE_FACTOR + SCALE_FACTOR_INC)
 	elif key == b'-':
-		
 		SCALE_FACTOR = max(SCALE_FACTOR_MIN, 
 				SCALE_FACTOR - SCALE_FACTOR_INC)
+	elif key == b'l':
+		X_OBJECT_ANGLE = (X_OBJECT_ANGLE + ROTATION_INC) % 360
+	elif key == b'j':
+		X_OBJECT_ANGLE = (X_OBJECT_ANGLE - ROTATION_INC) % 360
+	elif key == b'k':
+		Y_OBJECT_ANGLE = (Y_OBJECT_ANGLE + ROTATION_INC) % 360
+	elif key == b'i':
+		Y_OBJECT_ANGLE = (Y_OBJECT_ANGLE - ROTATION_INC) % 360
+	elif key == b'o':
+		Z_OBJECT_ANGLE = (Z_OBJECT_ANGLE + ROTATION_INC) % 360
+	elif key == b'u':
+		Z_OBJECT_ANGLE = (Z_OBJECT_ANGLE - ROTATION_INC) % 360
+	elif key == b't':
+		SHOW_AXIS = not SHOW_AXIS
 	elif key == b'\x1b': 
 		# ESC KEY
 		print('Program is now exiting...')
@@ -99,27 +126,109 @@ def inputEvents(key, x, y):
 
 	if ENABLE_RENDER:
 		ENABLE_RENDER=False
-		render(0)
+		render()
 
 def drawCube(edgeSize):
 	"""
 	"""
 
-def drawPentagonalPrims(BaseEdgeSize, Height):
+def drawPentagonalPrims(baseEdgeSize, height):
 	"""
 	Draws a Prims with a Pentagonal base.
+
+	Logic behind this implementation:
+
+	Draw a pentadiagonal 
+	function(x, y, l) { 
+	    points = NULL; 
+	    for (k in 72*(0:4)) points = rbind(points, 
+		c((x - l*0.5)*cos(0.4*pi*k), (y + tan(0.3*pi/2)*l)*sin(0.4*pi*k))); 
+	    return (points) 
+	}
+
+
+	A more direct but less readable implementation:
+	glVertex3f(-baseEdgeSize*0.5*math.cos(0.4*math.pi*k), 
+		math.tan(0.3*math.pi/2)*baseEdgeSize*math.sin(0.4*math.pi*k), 0)
 	"""
-    glBegin(GL_LINE_STRIP)
-
-    
-
-    glEnd()
+	
+	x=[]
+	y=[]
+	z=[]
+	for zShift in (1, -1):
+		glBegin(GL_LINE_STRIP)
+		for k in range(0, 6):
+			x.append(-baseEdgeSize*0.5*math.cos(0.4*math.pi*k))
+			y.append(math.tan(0.3*math.pi/2)*baseEdgeSize*math.sin(0.4*math.pi*k))
+			z.append(zShift*height/2.0)
+			glVertex3f(x[-1], y[-1], z[-1])
+		glEnd()
+	
+	for i in range(0, 6):
+		glBegin(GL_LINES)
+		glVertex3f(x[i], y[i], z[i])
+		glVertex3f(x[i+6], y[i+6], z[i+6])
+		glEnd()
 
 def drawHexagonalPyramid(radius, height):
 	"""
 	"""
 
 def drawObject(args):
+	"""
+	Call the function that draws the object via id
+	"""
+	id = args[0]
+	if id == 0: # Cube
+		drawCube(args[1])
+	elif id == 1: # Petagonal Prims
+		drawPentagonalPrims(args[1], args[2])
+	elif id == 2: # Hexagonal Pyramid
+		drawHexagonalPyramid(args[1])
+	else:
+		raise ValueError('First argument of \'args\'',
+			'must be a integer between 0, 1 or 2.')
+
+def drawAxis():
+	"""
+	Draw axis of the object
+	"""
+	AXIS_LIM = 1.0
+
+	glBegin(GL_LINES)
+
+	glColor3f(1, 0, 0)
+	glVertex3f(AXIS_LIM, 0, 0)
+	glVertex3f(-AXIS_LIM, 0, 0)
+	glVertex3f(AXIS_LIM-0.1, -0.025, -0.025)
+	glVertex3f(AXIS_LIM, 0, 0)
+	glVertex3f(AXIS_LIM-0.1, 0.025, 0.025)
+	glVertex3f(AXIS_LIM, 0, 0)
+
+	glColor3f(0, 1, 0)
+	glVertex3f(0, AXIS_LIM, 0)
+	glVertex3f(0, -AXIS_LIM, 0)
+	glVertex3f(-0.025, AXIS_LIM-0.1, -0.025)
+	glVertex3f(0, AXIS_LIM, 0)
+	glVertex3f(0.025, AXIS_LIM-0.1, 0.025)
+	glVertex3f(0, AXIS_LIM, 0)
+
+	glColor3f(0, 0, 1)
+	glVertex3f(0, 0, AXIS_LIM)
+	glVertex3f(0, 0, -AXIS_LIM)
+	glVertex3f(-0.025, -0.025, AXIS_LIM-0.025)
+	glVertex3f(0, 0, AXIS_LIM)
+	glVertex3f(0.025, 0.025, AXIS_LIM-0.025)
+	glVertex3f(0, 0, AXIS_LIM)
+
+	glEnd()
+
+def makeTransformations():
+	# 1. Load identity matrix
+	glLoadIdentity()
+
+	# 2. Load projection matrix
+	glOrtho(2, -2, 2, -2, 2, -100)
 	"""
 	Make all transformations here...
 	"""
@@ -130,32 +239,24 @@ def drawObject(args):
 	# glScale3f() must implement...
 	# glTranslate3f() must implement...
 
-	"""
-	Call the function that draws the object via id
-	"""
-	id = args[0]
-	if id == 0: # Cube
-		drawCube(args[1])
-	elif id == 1: # Petagonal Prims
-		drawPentagonalPrims(args[1])
-	elif id == 2: # Hexagonal Pyramid
-		drawHexagonalPyramid(args[1])
-	else:
-		raise ValueError('First argument of \'args\'',
-			'must be a integer between 0, 1 or 2.')
 
-def render(value):
+def render():
 	# 1. Clean buffers (Color and Depth buffers)
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
 
-	# 2. Load identity matrix
-	glLoadIdentity()
+	# 2. Make all transformations
+	makeTransformations()
 
-	# 3. Load projection matrix
-	glOrtho(2, -2, 2, -2, 2, -100)
+	# 3. draw axis
+	if SHOW_AXIS:
+		drawAxis()
+
+	# 3.a Set object color...
+	# idk what buttons should control these... to be implemented...
+	glColor3f(COLOR_R, COLOR_G, COLOR_B)
 
 	# 4. Draw object (passing the id)
-	drawObject(value)
+	drawObject(OBJECT_ARGUMENTS)
 
 	# 4.b Draw text
 	drawSubtitles()
@@ -167,7 +268,7 @@ def render(value):
 	# 	is done by glutSwapBuffers before it returns."
 	glutSwapBuffers()
 
-	# ...
+	# Enable another screen rendering call
 	global ENABLE_RENDER
 	ENABLE_RENDER=True
 
@@ -180,6 +281,6 @@ if __name__ == '__main__':
 	glutKeyboardFunc(inputEvents)
 
 	# Rendering function
-	render([0, 0])
+	render()
 
 	glutMainLoop()
